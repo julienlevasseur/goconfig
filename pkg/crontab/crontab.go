9df576c9c6f49f20d1692fb283e8a778a8a1655e
Julien Levasseur <Julien Levasseur>
2022-11-25 16:36:14 -0500

    Add cronab pkg

---
+++ pkg/crontab/crontab.go
@@ -0,0 +1,69 @@
+package crontab
+
+import (
+	"fmt"
+
+	"github.com/julienlevasseur/GoCfgMgr/pkg/file"
+	"github.com/julienlevasseur/GoCfgMgr/pkg/user"
+)
+
+// * * * * * command to be executed
+// | | | | |
+// | | | | |
+// | | | | |
+// | | | | |_______________ Day of the Week (0 - 6)(Sunday to Saturday)
+// | | | |
+// | | | |_______________ Month of the Year (1 - 12)
+// | | |
+// | | |_______________ Day of the Month (1 - 31)
+// | |
+// | |_______________ Hour (0 - 23)
+// |
+// |_______________ Minute (0 - 59)
+
+func Cron(
+	minute,
+	hour,
+	dayOfMonth,
+	monthOfYear,
+	dayOfWeek,
+	command string,
+) error {
+	username, err := user.Username()
+	if err != nil {
+		return err
+	}
+
+	// Check if entry already exists
+	found, err := file.LineIsPresent(
+		fmt.Sprintf("/var/spool/cron/crontabs/%s", username),
+		fmt.Sprintf(
+			"%s %s %s %s %s %s",
+			minute,
+			hour,
+			dayOfMonth,
+			monthOfYear,
+			dayOfWeek,
+			command,
+		),
+	)
+	if err != nil {
+		return err
+	}
+	if !found {
+		file.Append(
+			fmt.Sprintf("/var/spool/cron/crontabs/%s", username),
+			fmt.Sprintf(
+				"%s %s %s %s %s %s",
+				minute,
+				hour,
+				dayOfMonth,
+				monthOfYear,
+				dayOfWeek,
+				command,
+			),
+		)
+	}
+
+	return nil
+}
