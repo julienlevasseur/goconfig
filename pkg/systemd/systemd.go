b6ca87a34512ac8435044200d883520f68e4f2b0
Julien Levasseur <Julien Levasseur>
2022-12-01 10:02:10 -0500

    Improve systemd service tmpl

--- pkg/systemd/systemd.go
+++ pkg/systemd/systemd.go
@@ -25,35 +25,19 @@ func (s Service) New() {
 	content := `[Unit]
 {{if .Description}}Description={{ .Description}}{{end}}
 {{if .Requires}}Requires={{ .Requires}}{{end}}
-{{if .After}}
-After={{ .After}}
-{{end}}
+{{if .After}}After={{ .After}}{{end}}
 		
 [Service]
-{{if .Environment}}
-Environment={{ .Environment}}
-{{end}}
-{{if .Restart}}
-Restart={{ .Restart}}
-{{end}}
+{{if .Environment}}Environment={{ .Environment}}{{end}}
+{{if .Restart}}Restart={{ .Restart}}{{end}}
 
-{{if .ExecStart}}
-ExecStart={{ .ExecStart}}
-{{end}}
-{{if .ExecReload}}
-ExecReload={{ .ExecReload}}
-{{end}}
-{{if .ExecStop}}
-ExecStop={{ .ExecStop}}
-{{end}}
-{{if .KillSignal}}
-KillSignal={{ .KillSignal}}
-{{end}}
+{{if .ExecStart}}ExecStart={{ .ExecStart}}{{end}}
+{{if .ExecReload}}ExecReload={{ .ExecReload}}{{end}}
+{{if .ExecStop}}ExecStop={{ .ExecStop}}{{end}}
+{{if .KillSignal}}KillSignal={{ .KillSignal}}{{end}}
 
 [Install]
-{{if .WantedBy}}
-WantedBy={{ .WantedBy}}
-{{end}}
+{{if .WantedBy}}WantedBy={{ .WantedBy}}{{end}}
 `
 
 	err := file.Template(
