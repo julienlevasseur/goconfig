bc5cbc574805015b21c71e8b5dc882f47f43aa5e
Julien Levasseur <julien.levasseur@free.fr>
2024-06-20 11:54:12 -0400

    Improve TF install

--- pkg/softwares/terraform/terraform.go
+++ pkg/softwares/terraform/terraform.go
@@ -9,12 +9,13 @@ import (
 )
 
 const name = "Terraform"
+const destFolder = "destFolder"
 
 func Install(version, arch, platform string, notIf ...bool) {
 	if notIf[0] {
-		fmt.Printf("[%v][Install] Ignore Install due to NotIf", name)
+		fmt.Printf("[%v][Install] Ignore Install due to NotIf\n", name)
 	} else {
-		fmt.Printf("[%v][Install] Download archive", name)
+		fmt.Printf("[%v][Install] Download archive\n", name)
 		localFileName := fmt.Sprintf("terraform_%v_%v_%v.zip", version, platform, arch)
 		URL := fmt.Sprintf("https://releases.hashicorp.com/terraform/%v/terraform_%v_%v_%v.zip", version, version, platform, arch)
 
@@ -23,30 +24,52 @@ func Install(version, arch, platform string, notIf ...bool) {
 			localFileName,
 		)
 
-		// fmt.Printf("[%v][Install] Decompress archive", name)
-		// zip, err := os.Open(localFileName)
-		// if err != nil {
-		// 	panic(err)
-		// }
-		err := archive.Unzip(localFileName, "/usr/local/bin")
+		fmt.Printf("[%v][Install] Decompress archive\n", name)
+		err := archive.Unzip(localFileName, destFolder)
 		if err != nil {
 			panic(err)
 		}
 
-		fmt.Printf("[%v][Install] Delete archive", name)
+		fmt.Printf("[%v][Install] Delete archive\n", name)
 		err = file.Delete(localFileName)
 		if err != nil {
 			panic(err)
 		}
 
 		// Rename file to terraform_${version}
-		err = os.Rename("/usr/local/bin/terraform", fmt.Sprintf("/usr/local/bin/terraform_%v", version))
+		err = os.Rename(
+			fmt.Sprintf(
+				"%v/terraform",
+				destFolder,
+			),
+			fmt.Sprintf(
+				"%v/terraform_%v",
+				destFolder,
+				version,
+			),
+		)
 		if err != nil {
 			panic(err)
 		}
 
 		// Link terraform_${version} to terraform
-		err = os.Symlink(fmt.Sprintf("/usr/local/bin/terraform_%v", version), "/usr/local/bin/terraform")
+		err = os.Symlink(
+			fmt.Sprintf(
+				"%v/terraform_%v",
+				destFolder,
+				version,
+			),
+			fmt.Sprintf(
+				"%v/terraform",
+				destFolder,
+			),
+		)
+		if err != nil {
+			panic(err)
+		}
+
+		// Delete LICENSE.txt file (from archive)
+		err = file.Delete(fmt.Sprintf("%v/LICENSE.txt", destFolder))
 		if err != nil {
 			panic(err)
 		}
